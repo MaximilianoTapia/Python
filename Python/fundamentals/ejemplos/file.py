e21569d2c499b77baee70298d864111dd33c3f73
MaximilianoTapia <maxi.matc15@gmail.com>
2023-05-12 14:20:57 -0400

    commit de POO

--- Python/fundamentals/ejemplos/file.py
+++ Python/fundamentals/ejemplos/file.py
@@ -1,4 +1,4 @@
-# print(type(2))	
+# print(type(2))
 
 # decimal = 2.1
 
@@ -22,8 +22,8 @@
 # salida: {'name': 'Jack', 'edad': 38, 'peso: 160.2, 'usa_lentes': False, 'hobbies': ['escalada', 'programación']}
 # w = new_person.pop('peso')	# remueve la llave indicada y devuelve el valor
 # print(w)		# salida: 160.2
-# print(new_person)	
-# # salida: {'name': 'Jack', 'edad': 38, 'usa_lentes': False, 'hobbies': ['escalada', 'programación']}        
+# print(new_person)
+# # salida: {'name': 'Jack', 'edad': 38, 'usa_lentes': False, 'hobbies': ['escalada', 'programación']}
 
 #int_to_float = float(35)
 #float_to_int = int(44.2)
@@ -45,16 +45,48 @@
 #total = total + user_val		# salida: TypeError
 #total = total + int(user_val)		# el total será 61
 
-
+#
+# salida: 0 abc, 1 123, 2 xyz
+    
 #first_name = "Zen"
 #last_name = "Coder"
 #age = 27
 #print(f"Mi nombre is {first_name} {last_name} and tengo {age} años de edad.")
+    
+#
+# salida: abc, 123, xyz
 
 
-frutas = ['manzana', 'plátano', 'naranja', 'frutilla']
-vegetales = ['lechuga', 'pepino', 'zanahorias']
-frutas_y_vegetales = frutas + vegetales
-print(frutas_y_vegetales)
-ensalada = 3 * vegetales
-print(ensalada)
+#
+# salida: nombre, lenguaje
+
+
+#
+# otra forma de iterar a través de las claves
+#
+# salida: Washington, California, Idaho, Illinois, Texas, Oklahoma, Virginia
+# para iterar a través de los valores
+#
+# salida: Olympia, Sacramento, Boise, Springfield, Austin, Oklahoma City, Richmond
+# para iterar a través de las claves y valores
+#
+# salida: Washington = Olympia, California = Sacramento, Idaho = Boise, etc
+
+
+
+#
+
+
+#
+
+
+#
+# salida: c, a, d, n, a
+# nota: no hay e en el resultado, pero el bucle continuó hasta después de la e
+
+
+#		# solo se ejecuta en una salida limpia del bucle while (es decir, no un break)
+#
+# salida: 3, 2, 1
+
+
