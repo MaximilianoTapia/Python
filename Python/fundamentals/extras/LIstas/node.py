7e364e10ecd053d356a779b00419c589f7a603fb
Maximiliano Tapia <maxi.matc15@gmail.com>
2023-10-04 15:18:26 -0300

    z5

--- Python/fundamentals/extras/LIstas/node.py
+++ Python/fundamentals/extras/LIstas/node.py
@@ -1,15 +1,15 @@
 class SLNode:
-    def __init__(self, val):
-        self.value = val
+        def __init__(self, val):
+                self.value = val
         self.next = None
-    
-    def add_to_front(self, val):
-            new_node = SLNode(val)	# crea una nueva instancia de nuestra clase Node usando el valor dado
-    
-    def add_to_front(self, val):
-            new_node = SLNode(val)
-            current_head = self.head	# guarda el encabezado actual en una variable
-    
+
+        def add_to_front(self, val):
+                new_node = SLNode(val)	# crea una nueva instancia de nuestra clase Node usando el valor dado
+
+        def add_to_front(self, val):
+                new_node = SLNode(val)
+        current_head = self.head	# guarda el encabezado actual en una variable
+
         def add_to_front(self, val):
                 new_node = SLNode(val)
                 current_head = self.head
@@ -22,13 +22,4 @@ class SLNode:
 def print_values(self):
         runner = self.head
         while (runner != None):	# iterando mientras runner es un nodo y no None
-</div>
-
-
-
-
-
-
-
-
 
